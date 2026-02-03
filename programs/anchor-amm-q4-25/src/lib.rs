0bb26217b3902bdd6e4cd058ab7d38d20a919d12
Ayush <mail@ayuch.dev>
2026-02-03 18:29:39 +0530

    feat: implement swap and withdraw instructions
    Signed-off-by: Ayush <mail@ayuch.dev>

--- programs/anchor-amm-q4-25/src/lib.rs
+++ programs/anchor-amm-q4-25/src/lib.rs
@@ -5,7 +5,7 @@ mod instructions;
 mod state;
 
 use instructions::*;
-declare_id!("9FVkz5L9LZix4zXYmqJLzKBwRpm9aJ539J26UsZSrLWN");
+declare_id!("ATU5yg8yBUV1zBDWyzdQ4CMFmwAP5Mc64rFQnd5q8kYP");
 
 #[program]
 pub mod anchor_amm_q4_25 {
